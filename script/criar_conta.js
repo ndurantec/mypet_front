b67ef251ab95ba559372a4fb6d87d073cb6a5bdb
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2025-10-16 12:08:06 -0300

    30 commit

---
+++ script/criar_conta.js
@@ -0,0 +1,12 @@
+function criarContaUsuario(){
+    const nome = document.getElementById("nome").value;
+
+    const email = document.getElementById("email").value;
+
+    
+    const senha = document.getElementById("senha").value;
+
+    const telefone = document.getElementById("telefone").value;
+
+    
+}
