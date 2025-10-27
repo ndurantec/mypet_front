cc70b60b62e3adea8e9e39a477c66afde0a584a5
angellyne <angellyne.marques@escola.pr.gov.br>
2025-10-27 08:26:14 -0300

    fiz o fetch

--- script/loja.js
+++ script/loja.js
@@ -11,13 +11,79 @@ function validarNome() {
         return "Digite pelo menos 3 caracteres";
     }
 
-    return ""; 
-
+    return "";  
 }
 
+const email = document.getElementById('email').value;
+    if (email === '') {
+        mostrarErro('erro-email', 'Informe o e-mail.');
+        ok = false;
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+        mostrarErro('erro-email', 'E-mail inválido.');
+        ok = false;
+    }
 
 function mostrarErro(idCampo, mensagem) {
   document.getElementById(idCampo).textContent = mensagem;
 }
 
+function salvarloja() {
+   
 
+
+    // Envia os dados via fetch
+    fetch("http://127.0.0.1:8080/responsaveis", { // altere a URL conforme seu endpoint
+       
+    }).then(response => {
+           
+    }).then(data => {
+       
+    }).catch(error => {
+       
+    });
+}
+function consultarloja() {
+   
+
+
+    // Envia os dados via fetch
+    fetch("http://127.0.0.1:8080/responsaveis", { // altere a URL conforme seu endpoint
+       
+    }).then(response => {
+           
+    }).then(data => {
+       
+    }).catch(error => {
+       
+    });
+}
+function salvardeletar() {
+   
+
+
+    // Envia os dados via fetch
+    fetch("http://127.0.0.1:8080/responsaveis", { // altere a URL conforme seu endpoint
+       
+    }).then(response => {
+           
+    }).then(data => {
+       
+    }).catch(error => {
+       
+    });
+}
+function salvaralterar() {
+   
+
+
+    // Envia os dados via fetch
+    fetch("http://127.0.0.1:8080/responsaveis", { // altere a URL conforme seu endpoint
+       
+    }).then(response => {
+           
+    }).then(data => {
+       
+    }).catch(error => {
+       
+    });
+}
