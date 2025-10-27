e1e81c7059ab3826ed78c4153fa1043cd3e63903
nanojha12 <jose.altissimo@escola.pr.gov.br>
2025-10-27 08:28:25 -0300

    função fetch

--- script/endereco.js
+++ script/endereco.js
@@ -28,3 +28,74 @@ function enviarCadastro() {
 
          alert( rua + " - " + numero + " - " + bairro + " - " + cep + " - " + complemento);
 }
+
+
+
+
+function cadastraendereco() {
+   
+
+
+    // Envia os dados via fetch
+    fetch('http://127.0.0.1:8080/responsaveis', { // altere a URL conforme seu endpoint
+       
+    }).then(response => {
+           
+    }).then(data => {
+       
+    }).catch(error => {
+       
+    });
+}
+
+
+
+function consultarendereco() {
+   
+
+
+    // Envia os dados via fetch
+    fetch('http://127.0.0.1:8080/responsaveis', { // altere a URL conforme seu endpoint
+       
+    }).then(response => {
+           
+    }).then(data => {
+       
+    }).catch(error => {
+       
+    });
+}
+
+
+function deletarendereco() {
+   
+
+
+    // Envia os dados via fetch
+    fetch('http://127.0.0.1:8080/responsaveis', { // altere a URL conforme seu endpoint
+       
+    }).then(response => {
+           
+    }).then(data => {
+       
+    }).catch(error => {
+       
+    });
+}
+
+
+function atualizarendereco() {
+   
+
+
+    // Envia os dados via fetch
+    fetch('http://127.0.0.1:8080/responsaveis', { // altere a URL conforme seu endpoint
+       
+    }).then(response => {
+           
+    }).then(data => {
+       
+    }).catch(error => {
+       
+    });
+}
