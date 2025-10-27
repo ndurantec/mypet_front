bb3a921c9c5dd95832ce50c11320b1922dada225
JuliaEffko <julia.vendruscolo@escola.pr.gov.br>
2025-10-27 08:34:39 -0300

    Agenda atualizada

--- script/agenda.js
+++ script/agenda.js
@@ -1,39 +1,68 @@
-function agendarTosa() {
-    const tipoTosa = document.getElementById("tipoTosa").value;
-    const dataTosa = document.getElementById("dataTosa").value;
+function Salvarservico() {
+   
 
-    alert("Tosa agendada com sucesso!");
-    console.log("Tosa agendada:");
-    console.log("Tipo de Tosa:", tipoTosa);
-    console.log("Data e Horário:", dataTosa);
+
+    // Envia os dados via fetch
+    fetch('http://127.0.0.1:8080/responsaveis', { // altere a URL conforme seu endpoint
+       
+    }).then(response => {
+           
+    }).then(data => {
+       
+    }).catch(error => {
+       
+    });
 }
 
-function agendarBanho() {
-    const tipoBanho = document.getElementById("tipoBanho").value;
-    const dataBanho = document.getElementById("dataBanho").value;
 
-    alert("Banho agendado com sucesso!");
-    console.log("Banho agendado:");
-    console.log("Tipo de Banho:", tipoBanho);
-    console.log("Data e Horário:", dataBanho);
+function Consultarservico() {
+   
+
+
+    // Envia os dados via fetch
+    fetch('http://127.0.0.1:8080/responsaveis', { // altere a URL conforme seu endpoint
+       
+    }).then(response => {
+           
+    }).then(data => {
+       
+    }).catch(error => {
+       
+    });
 }
 
-function agendarCheckup() {
-    const tipoCheckup = document.getElementById("tipoCheckup").value;
-    const dataCheckup = document.getElementById("dataCheckup").value;
 
-    alert("Check-up agendado com sucesso!");
-    console.log("Check-up agendado:");
-    console.log("Tipo de Check-up:", tipoCheckup);
-    console.log("Data e Horário:", dataCheckup);
+
+function Alterarservico() {
+   
+
+
+    // Envia os dados via fetch
+    fetch('http://127.0.0.1:8080/responsaveis', { // altere a URL conforme seu endpoint
+       
+    }).then(response => {
+           
+    }).then(data => {
+       
+    }).catch(error => {
+       
+    });
 }
 
-function agendarProfissional() {
-    const profissional = document.getElementById("selecaoProfissional").value;
-    const dataProfissional = document.getElementById("dataProfissional").value;
 
-    alert("Agendamento com profissional realizado com sucesso!");
-    console.log("Agendamento com profissional:");
-    console.log("Profissional:", profissional);
-    console.log("Data e Horário:", dataProfissional);
+
+function agendarservico() {
+   
+
+
+    // Envia os dados via fetch
+    fetch('http://127.0.0.1:8080/responsaveis', { // altere a URL conforme seu endpoint
+       
+    }).then(response => {
+           
+    }).then(data => {
+       
+    }).catch(error => {
+       
+    });
 }
