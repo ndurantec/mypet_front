1d50937917de5acd1bf8a245493cdb96c2765914
nanojha12 <jose.altissimo@escola.pr.gov.br>
2025-10-27 10:22:48 -0300

    validado

--- script/endereco.js
+++ script/endereco.js
@@ -34,10 +34,37 @@ function enviarCadastro() {
 
 function cadastraendereco() {
    
+    let rua = document.getElementById("rua").value;
+    let numero = document.getElementById("numero").value;
+    let bairro = document.getElementById("bairro").value;
+    let cep = document.getElementById("cep").value;
+    let complemento = document.getElementById("complemento").value;
+    
+    if(rua == ""){
+        alert("Você precisa preencher o campo rua");
+    }
+
+    if(numero == ""){
+        alert("Você precisa preencher o campo numero");
+    }
+
+    if(bairro == ""){
+        alert("Você precisa preencher o campo bairro");
+    }
+
+    if( cep== ""){
+        alert("Você precisa preencher o campo cep");
+    }
+
+      if( complemento== ""){
+        alert("Você precisa preencher o campo complemento");
+    }
 
 
-    // Envia os dados via fetch
-    fetch('http://127.0.0.1:8080/responsaveis', { // altere a URL conforme seu endpoint
+         alert( rua + " - " + numero + " - " + bairro + " - " + cep + " - " + complemento);
+
+   
+    fetch('http://127.0.0.1:8080/responsaveis', { 
        
     }).then(response => {
            
@@ -54,8 +81,8 @@ function consultarendereco() {
    
 
 
-    // Envia os dados via fetch
-    fetch('http://127.0.0.1:8080/responsaveis', { // altere a URL conforme seu endpoint
+   
+    fetch('http://127.0.0.1:8080/responsaveis', { 
        
     }).then(response => {
            
@@ -71,8 +98,8 @@ function deletarendereco() {
    
 
 
-    // Envia os dados via fetch
-    fetch('http://127.0.0.1:8080/responsaveis', { // altere a URL conforme seu endpoint
+   
+    fetch('http://127.0.0.1:8080/responsaveis', { 
        
     }).then(response => {
            
@@ -88,8 +115,8 @@ function atualizarendereco() {
    
 
 
-    // Envia os dados via fetch
-    fetch('http://127.0.0.1:8080/responsaveis', { // altere a URL conforme seu endpoint
+   
+    fetch('http://127.0.0.1:8080/responsaveis', { 
        
     }).then(response => {
            
