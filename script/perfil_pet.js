951bde0071f6d3a3cb6d1ab73c5ab359f0b06b82
MarcelTryHard <o.marcel@escola.pr.gov.br>
2025-11-06 12:02:03 -0300

    feito

--- script/perfil_pet.js
+++ script/perfil_pet.js
@@ -54,92 +54,110 @@ function coletarDados() {
 }
 
 function salvarCadastro() {
-    if (!validarFormulario()) {
-        return;
-    }
-
+    
     limparErros();
+    
+    if (!validarFormulario()) return;
 
     const dados = coletarDados();
-    console.log("Enviando cadastro:", dados);
+
+    var headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    headers.append("Access-Control-Allow-Origin", "*");
 
     fetch('http://127.0.0.1:8080/pet/cadpet', {
+
         method: 'POST',
         mode: 'cors',
         cache: 'no-cache',
-        body: JSON.stringify(dados),
+        body: JSON.stringify(
+            dados
+        ),
+    
         headers: headers
     })
         .then(async response => {
-            let data = await response.json();
+      let data = await response.json();
 
-            console.log(data);
+      console.log(data);
+      
 
+      if (!response.ok) {
+        // Caso sejam erros de validação no DTO
+        if (typeof data === "object") {
+          let mensagens = Object.values(data).join("<br>");
 
-            if (!response.ok) {
-                // Caso sejam erros de validação no DTO
-                if (typeof data === "object") {
-                    let mensagens = Object.values(data).join("<br>");
+          console.log("Entrou dento do if data ==== object");
+          console.log("----------------------------------------------");
+          console.log(mensagens);
+          console.log("----------------------------------------------");
 
-                    console.log("Entrou dento do if data ==== object");
-                    console.log("----------------------------------------------");
-                    console.log(mensagens);
-                    console.log("----------------------------------------------");
+            let mensagensGlobais = []; // Para erros que não mapeiam para um campo específico
 
-                    let mensagensGlobais = []; // Para erros que não mapeiam para um campo específico
+            for (const [campo, mensagem] of Object.entries(data)) {
+                // Mapeia o nome do campo do backend ('cpf', 'email', etc.) para o ID do elemento no HTML
+                const idElementoErro = "erro-" + campo; // Ex: 'cpf_error_message'
 
-                    for (const [campo, mensagem] of Object.entries(data)) {
-                        // Mapeia o nome do campo do backend ('cpf', 'email', etc.) para o ID do elemento no HTML
-                        const idElementoErro = "erro-" + campo; // Ex: 'cpf_error_message'
-
-                        console.log("========================================================");
-                        console.log(idElementoErro);
-                        console.log("========================================================");
-                        // Tenta exibir o erro no elemento específico
-                        if (document.getElementById(idElementoErro)) {
-                            //CHAMANDO A SUA FUNÇÃO mostrarErro(idElemento, mensagem)
-                            mostrarErro(idElementoErro, mensagem);
-
-                        }
-
-
-
-                    }
+                console.log("========================================================");
+                console.log(idElementoErro);
+                console.log("========================================================");
+                // Tenta exibir o erro no elemento específico
+                if (document.getElementById(idElementoErro)) {
+                    //CHAMANDO A SUA FUNÇÃO mostrarErro(idElemento, mensagem)
+                    mostrarErro(idElementoErro, mensagem);
+                                        
+                } 
 
 
-                } else {
-                    mostrarMensagem("⚠️ Erro desconhecido", "erro");
-                }
-                throw new Error("Erro de validação");
+
             }
 
-            return data;
-        })
-        .then(data => {
-            if (data.id) {
-                localStorage.setItem("id_usuario", data.id);
-                // mostrarMensagem(data.message || "✅ Usuario cadastrado com sucesso!", "sucesso");
-            }
-        })
-        .catch(error => console.error(error));
+          
+        } else {
+          mostrarMensagem("⚠️ Erro desconhecido", "erro");
+        }
+        throw new Error("Erro de validação");
+      }
+
+      return data;
+    })
+    .then(data => {
+      if (data.id) {
+        localStorage.setItem("id_usuario", data.id);
+        // mostrarMensagem(data.message || "✅ Usuario cadastrado com sucesso!", "sucesso");
+      }
+    })
+    .catch(error => console.error(error));
 }
 
 function consultarCadastro() {
 
-
-
-    const nome = document.getElementById("nome").value.trim();
-
-    if (!nome) {
-        return;
-    }
-
     limparErros();
 
+
+    if (!validarFormulario()) return;
+
+    const dados = coletarDados();
+    //console.log("Enviando criar conta:", dados);
+
+    console.log(JSON.stringify(dados));
+
+    var headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    headers.append("Access-Control-Allow-Origin", "*");
+
+
+
     fetch(`http://127.0.0.1:8080/pet/buscarNomePet/${encodeURIComponent(nome)}`, {
-        method: 'GET',
+        method: 'POST',
         mode: 'cors',
+        cache: 'no-cache',
+        body: JSON.stringify(
+            dados
+        ),
+    
         headers: headers
+
     })
         .then(async response => {
             let data = await response.json();
@@ -197,23 +215,25 @@ function consultarCadastro() {
 
 function deletarCadastro() {
 
-
-
-
-    const nome = document.getElementById("nome").value.trim();
-
-    if (!nome) {
-
-        return;
-    }
-
-    if (!confirm(`Tem certeza que deseja deletar o cadastro de ${nome}?`)) {
-        return;
-    }
-
     limparErros();
 
+
+    if (!validarFormulario()) return;
+
+    const dados = coletarDados();
+    //console.log("Enviando criar conta:", dados);
+
+    console.log(JSON.stringify(dados));
+
+    var headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    headers.append("Access-Control-Allow-Origin", "*");
+
+
+    
+
     fetch('http://127.0.0.1:8080/pet/apagar/', {
+
         method: 'DELETE',
         mode: 'cors',
         body: JSON.stringify({ nome: nome }),
@@ -275,18 +295,21 @@ function deletarCadastro() {
 
 function alterarCadastro() {
 
-
-
-    if (!validarFormulario()) {
-
-        return;
-    }
-
     limparErros();
 
+    if (!validarFormulario()) return;
+
     const dados = coletarDados();
+    //console.log("Enviando criar conta:", dados);
+
+    console.log(JSON.stringify(dados));
+
+    var headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    headers.append("Access-Control-Allow-Origin", "*");
 
     fetch('http://127.0.0.1:8080/pet/alterar/', {
+        
         method: 'PUT',
         mode: 'cors',
         body: JSON.stringify(dados),
