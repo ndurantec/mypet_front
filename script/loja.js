1847edd404651fd5b37418e40b2e1e7dc9d61ad8
angellyne <angellyne.marques@escola.pr.gov.br>
2025-11-07 11:18:20 -0300

    fazendo o js da loja

--- script/loja.js
+++ script/loja.js
@@ -3,13 +3,16 @@ function validarFormulario() {
   
     let nome = document.getElementById("nome").value;
     let cnpj = document.getElementById("cnpj").value;
-    
+    let telefone = document.getElementById("telefone").value;
+    let email = document.getElementById("email").value;
+    let endereco = document.getElementById("endereço").value;
     let ok = true;
 
     if (!nome) { mostrarErro('erro-nome', 'Verifique se possui nome para continuar.'); ok = false; }
     if (!cnpj) { mostrarErro('erro-cnpj', 'Verifique se possui cnpj para continuar.'); ok = false; }
-    
-
+    if (!telefone) { mostrarErro('erro-telefone', 'Verifique se possui telefone para continuar.'); ok = false; }
+    if (!email) { mostrarErro('erre-mail', 'Verifique se possui email para continuar.'); ok = false; }
+    if (!endereco) { mostrarErro('erro-endereco', 'Verifique se possui endereço para continuar.'); ok = false; }
     return ok;
 }
 
@@ -24,35 +27,38 @@ function coletarDados() {
   
     return {
         nome: document.getElementById("nome").value.trim(),
-        cnpj: document.getElementById("cnpj").value.trim()
+        cnpj: document.getElementById("cnpj").value.trim(),
+        telefone: document.getElementById("telefone").value.trim(),
+        email: document.getElementById("email").value.trim(),
+        endereco: document.getElementById("endereco").value.trim()
     };
 }
 
 
-function validarNome() {
+// function validarNome() {
 
-    const nome = document.getElementById('nome').value;
+//     const nome = document.getElementById('nome').value;
     
-    if (nome.trim() === "") {        
-        mostrarErro('erro-nome', 'O nome deve ter pelo menos 3 caracteres.');
-    }
+//     if (nome.trim() === "") {        
+//         mostrarErro('erro-nome', 'O nome deve ter pelo menos 3 caracteres.');
+//     }
 
-    if (nome.length < 3) {
-        return "Digite pelo menos 3 caracteres";
-    }
+//     if (nome.length < 3) {
+//         return "Digite pelo menos 3 caracteres";
+//     }
 
-    const email = document.getElementById('email').value;
-    if (email === '') {
-        mostrarErro('erro-email', 'Informe o e-mail.');
-        ok = false;
-    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-        mostrarErro('erro-email', 'E-mail inválido.');
-        ok = false;
-    }
-}
+//     const email = document.getElementById('email').value;
+//     if (email === '') {
+//         mostrarErro('erro-email', 'Informe o e-mail.');
+//         ok = false;
+//     } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+//         mostrarErro('erro-email', 'E-mail inválido.');
+//         ok = false;
+//     }
+// }
 
 
-function cadastrarloja() {
+function cadastrar() {
 
     limparErros();
 
@@ -157,18 +163,18 @@ function cadastrarloja() {
 }
 
 
-function coletarDados() {
+// function coletarDados() {
     
-    const canvas = document.getElementById('signaturePad');
+//     const canvas = document.getElementById('signaturePad');
   
-    return {
-        nome: document.getElementById("nome").value.trim(),
-        cnpj: document.getElementById("cnpj").value.trim()
-    };
-}
+//     return {
+//         nome: document.getElementById("nome").value.trim(),
+//         cnpj: document.getElementById("cnpj").value.trim()
+//     };
+// }
 
 
-function alterarloja() {
+function alterar() {
 
     limparErros();
 
@@ -245,13 +251,13 @@ function alterarloja() {
         // mostrarMensagem(data.message || "✅ Usuario cadastrado com sucesso!", "sucesso");
       }
     })
-    .catch(error => console.error(error));
+    .catch(error => console.error(error))
 
    
     });
 }
 
-function deletarloja() {
+function deletar() {
      
     limparErros();
 
@@ -327,12 +333,12 @@ function deletarloja() {
         // mostrarMensagem(data.message || "✅ Usuario cadastrado com sucesso!", "sucesso");
       }
     })
-    .catch(error => console.error(error));
+    .catch(error => console.error(error))
     });
 }
 
  
-function consultarloja() {
+function consultar() {
    
     limparErros();
 
@@ -412,6 +418,6 @@ function consultarloja() {
         // mostrarMensagem(data.message || "✅ Usuario cadastrado com sucesso!", "sucesso");
       }
     })
-    .catch(error => console.error(error));
+    .catch(error => console.error(error))
     });
 }
