887af8efc292dea0b10d17c97b9ba66f6c399421
nanojha12 <jose.altissimo@escola.pr.gov.br>
2025-11-10 12:11:33 -0300

    10

--- script/endereco.js
+++ script/endereco.js
@@ -7,37 +7,6 @@ function limparErros() {
     erros.forEach(e => e.textContent = '');
 }
 
-function enviarCadastro() { 
-    let rua = document.getElementById("rua").value;
-    let numero = document.getElementById("numero").value;
-    let bairro = document.getElementById("bairro").value;
-    let cep = document.getElementById("cep").value;
-    let complemento = document.getElementById("complemento").value;
-    
-    if(rua == ""){
-        alert("Você precisa preencher o campo rua");
-    }
-
-    if(numero == ""){
-        alert("Você precisa preencher o campo numero");
-    }
-
-    if(bairro == ""){
-        alert("Você precisa preencher o campo bairro");
-    }
-
-    if( cep== ""){
-        alert("Você precisa preencher o campo cep");
-    }
-
-      if( complemento== ""){
-        alert("Você precisa preencher o campo complemento");
-    }
-
-
-         alert( rua + " - " + numero + " - " + bairro + " - " + cep + " - " + complemento);
-}
-
 function validarFormulario() {
  
     let rua = document.getElementById("rua").value;
